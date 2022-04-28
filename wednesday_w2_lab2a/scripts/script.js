5ac59970665d2f2a4592adf949e4c62457009a39
Gike93 <102712520+Gike93@users.noreply.github.com>
2022-04-28 01:18:56 -0600

    Add files via upload

---
+++ wednesday_w2_lab2a/scripts/script.js
@@ -0,0 +1,9 @@
+/*
+    Michael Robins
+    File Name: script.js
+    4/28/2022 
+*/
+//jQuery for hero image to consume the header windo space
+    $(document).ready(function(){
+        $('.hero').height($(window).height());
+    });
